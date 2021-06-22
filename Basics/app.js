ab14eed8d312455785b279a813138b229af2d1a6
Siana Rizwan <sianarizwan@gmail.com>
2021-06-22 22:46:06 +0600

    .get and .use() methods added in app.js

--- Basics/app.js
+++ Basics/app.js
@@ -6,5 +6,16 @@ const app = express();
 app.get("/", (req,res)=>{
     res.send("<H1> Home Page </H1>");
 });
+app.get("/about", (req,res)=>{
+    res.send("<H1> About Page </H1>");
+});
+app.get("/contact", (req,res)=>{
+    res.send("<H1> Contact Page </H1>");
+});
+
+//if navigated to any other url except the specified ones then the following error will be shown
+app.use((req,res)=>{
+    res.send("<H1> Error 404! Page doesn't exist </H1>");
+});
 
 module.exports = app;
