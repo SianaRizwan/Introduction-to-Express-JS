e63b90f40fdb10c15d8cf07e57b0a3e2d201f314
Siana Rizwan <sianarizwan@gmail.com>
2021-06-23 13:22:03 +0600

    Controllers extracted

--- Basics/routes/userRoutes.routes.js
+++ Basics/routes/userRoutes.routes.js
@@ -2,38 +2,21 @@ const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
 const isLoggedIn = require("./../middlewares/auth.middlewares")
+const {
+    getRegister,
+    postRegister,
+    getLogin,
+    getDashboard,
+  } = require("./../controllers/userController.controllers");
+  
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-router.get("/login", (req,res)=>{
-    const id = req.query.id;
-    res.send(`User with ID - ${id} is requesting to login`);
+router.get("/login", getLogin);
 
-    //res.send("<H1> This is Login Page </H1>");
-});
+router.get("/dashboard", getDashboard);
 
-
-router.get("/register", (req,res)=>{
-    //res.send("<H1> This is Register Page </H1>");
-    res.sendFile("register.html", {
-        root: "./views/users"
-    });
-});
-router.get("/dashboard", (req,res)=>{
-    res.send("User Dashboard");
-})
-router.post("/register", isLoggedIn, (req,res)=>{
-    // const username = req.body.username;
-    // const email = req.body.email;
-    // res.send(
-    //          `<H1>user with Email - ${email} and Username - ${username} is requesting to login.</H1>`
-    //        );
-    // res.sendFile("register.html", {
-    //     root: "./views/users"
-    // });
-
-    res.redirect("/dashboard");
-});
+router.route("/register").all(isLoggedIn).get(getRegister).post(postRegister);
 
 module.exports = router;
